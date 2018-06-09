f9b70945432cbed46ca2ccf90dd2f7f32782308e
omochimetaru <omochi.metaru@gmail.com>
2018-06-09 10:20:47 +0900

    carthage

--- ForceHTTP/FHTTPMethod.swift
+++
@@ -1,8 +0,0 @@
-public enum FHTTPMethod : String, CustomStringConvertible {
-    case get = "GET"
-    case post = "POST"
-    
-    public var description: String {
-        return self.rawValue
-    }
-}
